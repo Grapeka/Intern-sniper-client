7376169227a5608681839df7eb29758665b05874
melon <momobp114@gmail.com>
2022-11-26 14:28:03 +0700

    feat: authProvider auth type

--- src/providers/authProvider.tsx
+++ src/providers/authProvider.tsx
@@ -1,12 +1,8 @@
+import User from '../types/User'
 import { createContext } from "react";
 
 export const AuthContext = createContext<{
-  auth: {
-    id: string | null;
-    email: string | null;
-    profilePicture: string | null;
-    role: string | null;
-  } | null,
+  auth: User | null,
   token: string | null,
   setToken: React.Dispatch<React.SetStateAction<string | null>>
 } | null>(null)
